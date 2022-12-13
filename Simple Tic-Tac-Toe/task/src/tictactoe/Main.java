d9ea33bb82fead273254904dccaa5ae57ffd2a30
Edwin Silva Home <edwinsils@hotmail.com>
2022-12-13 16:59:53 -0500

    Finish the stage 2/5 for the project -Simple Tic Tac Toe

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,12 +1,15 @@
 package tictactoe;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         // write your code here
-        System.out.print("""
-                X O X
-                O X O
-                X X O
-                """);
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.next();
+        System.out.println("---------");
+        System.out.println("| " + string.charAt(0) + " " + string.charAt(1) + " " + string.charAt(2) + " |");
+        System.out.println("| " + string.charAt(3) + " " + string.charAt(4) + " " + string.charAt(5) + " |");
+        System.out.println("| " + string.charAt(6) + " " + string.charAt(7) + " " + string.charAt(8) + " |");
+        System.out.println("---------");
     }
 }
