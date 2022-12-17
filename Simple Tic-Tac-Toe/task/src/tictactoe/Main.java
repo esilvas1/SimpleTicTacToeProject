f083c68f8de56fb108f6e0c2360c6e1c2b0b5ccc
Edwin Silva Home <edwinsils@hotmail.com>
2022-12-17 07:57:21 -0500

    Finished and solved the stage 4/5 of project TicTacToe

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,15 +1,87 @@
 package tictactoe;
 import java.util.Scanner;
+import java.util.SimpleTimeZone;
 
 public class Main {
     public static void main(String[] args) {
         // write your code here
-        String[][] matrix = printGame();
+        Scanner scanner = new Scanner(System.in);
+        String empty = " ";
+        System.out.print("Insert string of the game: >");
+        String string = scanner.next();
+        String[][] matrix = printGame(string);
+        /*
         if (isThereWinner(matrix) && !isGameImpossible(matrix)) System.out.println(winner(matrix) + " wins");
         else if (isGameImpossible(matrix)) System.out.println("Impossible");
         else if (withSpace(matrix)) System.out.println("Game not finished");
         else if (isDraw(matrix)) System.out.println("Draw");
+         */
+        System.out.print("Enter coordinates (x y): >");
+        string = scanner.nextLine();
+        string = validateToNumbers(string);
+        string = validateToCoordiantes(string);
+        string = cellIsOccupied(string,matrix);
+        string = modifyMatrix(string,matrix);
+        matrix = printGame(string);
     }
+
+    public static String modifyMatrix(String string, String[][] matrix) {
+        StringBuilder sb = new StringBuilder();
+        int x = Integer.valueOf(String.valueOf(string.charAt(0))) - 1;
+        int y = Integer.valueOf(String.valueOf(string.charAt(2))) - 1;
+        matrix[x][y] = "X";
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                sb.append(matrix[i][j]);
+            }
+        }
+        string = sb.toString();
+        return string;
+    }
+    public static String validateToNumbers(String string) {
+        String empty = " ";
+        Scanner scanner = new Scanner(System.in);
+        while (string.length() != 3
+                || !Character.isDigit(string.charAt(0))
+                || !empty.equals(String.valueOf(string.charAt(1)))
+                || !Character.isDigit(string.charAt(2))) {
+            System.out.println("You should enter numbers!");
+            //System.out.print("> ");
+            string = scanner.nextLine();
+        }
+        return string;
+    }
+
+    public static String validateToCoordiantes(String string) {
+        Scanner scanner = new Scanner(System.in);
+        while (Integer.valueOf(String.valueOf(string.charAt(0))) <= 0
+                || Integer.valueOf(String.valueOf(string.charAt(0))) > 3
+                || Integer.valueOf(String.valueOf(string.charAt(2))) <= 0
+                || Integer.valueOf(String.valueOf(string.charAt(2))) > 3) {
+            System.out.println("Coordinates should be from 1 to 3!");
+            //System.out.print("> ");
+            string = scanner.nextLine();
+            string = validateToNumbers(string);
+        }
+        return string;
+    }
+
+    public static String cellIsOccupied(String string, String[][] matrix) {
+        Scanner scanner = new Scanner(System.in);
+        int x = Integer.valueOf(String.valueOf(string.charAt(0))) - 1;
+        int y = Integer.valueOf(String.valueOf(string.charAt(2))) - 1;
+        while (!(matrix[x][y].equals("_"))) {
+            System.out.println("This cell is occupied! Choose another one!");
+            //System.out.print("> ");
+            string = scanner.nextLine();
+            string = validateToNumbers(string);
+            string = validateToCoordiantes(string);
+            x = Integer.valueOf(String.valueOf(string.charAt(0))) - 1;
+            y = Integer.valueOf(String.valueOf(string.charAt(2))) - 1;
+        }
+        return string;
+    }
+
 
     public static boolean withSpace(String[][] matrix) {
         boolean flag = false;
@@ -79,10 +151,7 @@ public class Main {
         return flag;
     }
 
-    public static String[][] printGame() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Insert string of the game: >");
-        String string = scanner.next();
+    public static String[][] printGame(String string) {
         System.out.println("---------");
         System.out.println("| " + string.charAt(0) + " " + string.charAt(1) + " " + string.charAt(2) + " |");
         System.out.println("| " + string.charAt(3) + " " + string.charAt(4) + " " + string.charAt(5) + " |");
